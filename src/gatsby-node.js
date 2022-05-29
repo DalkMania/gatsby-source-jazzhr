f75628b6daaad3e89b19761783e1067b4619a39d
Niklas Dahlqvist <dalkmania@gmail.com>
2022-05-29 14:11:58 -0700

    Recursive axios call

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,7 +1,20 @@
+import { getApiData } from "./utils/fetch";
+
+const typePrefix = `jazzhr__`;
+
 export const createSchemaCustomization = async ({ actions }) => {
     const { createTypes } = actions;
 };
 
-export const sourceNodes = async ({ actions, getNode, store, cache, createNodeId, createContentDigest }) => {
+export const sourceNodes = async (
+    { actions, getNode, store, cache, createNodeId, createContentDigest },
+    { apiKey, verboseOutput }
+) => {
     const { createNode } = actions;
+
+    let entities = await getApiData({
+        apiKey,
+        verboseOutput,
+        typePrefix
+    });
 };
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,22 +1,33 @@
-import axios from `axios`
-import httpExceptionHandler from `./http-exception-handler`
+import axios from "axios";
+import httpExceptionHandler from "./http-exception-handler";
 
 /**
  * High-level function to coordinate fetching data from the JazzHR API.
  */
-const fetch = async ({ apiKey, verbose, typePrefix }) => {
-    let entities = []
-  try {
-    const res = await axios({
-      method: `get`,
-      url: `https://api.resumatorapi.com/v1/jobs?apikey=${apiKey}`,
-    })
-    entities = res.data
-  } catch (e) {
-    httpExceptionHandler(e)
-  }
+const fetch = (apiKey, page) => {
+    try {
+        const res = axios({
+            method: `get`,
+            url: `https://api.resumatorapi.com/v1/jobs/page/${page}?apikey=${apiKey}`
+        });
+        return res;
+    } catch (e) {
+        httpExceptionHandler(e);
+    }
+};
 
-  return entities
-}
+/**
+ * Recursive function to coordinate fetching data from the JazzHR API.
+ */
+export const getApiData = async ({ apiKey, verbose, typePrefix, page = 1 }) => {
+    let entities = [];
 
-export default fetch;
+    return fetch(apiKey, page)
+        .then((res) => {
+            entities = entities.concat(res.data);
+            return fetch(apiKey, page + 1);
+        })
+        .catch(() => {
+            return entities;
+        });
+};
